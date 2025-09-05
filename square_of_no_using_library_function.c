601512d7f9b75e0cc9296b1347b22bf24d54f8bc
Anubhav Kumar Singh <182789693+DarkEden052@users.noreply.github.com>
2025-09-05 12:48:13 +0530

    Add files via upload

---
+++ square_of_no_using_library_function.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+int n,s;
+printf("Enter the Number  :");
+scanf("%d",&n);
+s=pow(n,2);
+printf("Sqaure of n is  :%d\n",s);
+return 0;
+}
